5a9bcbacb4c07c2b28f00663a3e59311f123e7d1
nOSkr <118327643+nOSkr@users.noreply.github.com>
2024-01-02 13:04:30 +0800

    sync

--- routes/users.js
+++ routes/users.js
@@ -35,10 +35,10 @@ router.route("/loginCheckPassword").post(protect, loginCheckPassword);
 router.route("/registerCheckPhone").post(registerPhone);
 router.route("/registerCheckPassword").post(protect, registerPassword);
 router.route("/logout").get(logout);
-router.route("/callbacks/:id/:numId").get(chargeTime);
-router.route("/check/challbacks/:id/:numId").get(invoiceCheck);
-router.route("/callbacks/gift/:id/:numId/:phone").get(chargeGift);
-router.route("/check/challbacks/gift/:id/:numId/:phone").get(invoiceGiftCheck);
+// router.route("/callbacks/:id/:numId").get(chargeTime);
+// router.route("/check/challbacks/:id/:numId").get(invoiceCheck);
+// router.route("/callbacks/gift/:id/:numId/:phone").get(chargeGift);
+// router.route("/check/challbacks/gift/:id/:numId/:phone").get(invoiceGiftCheck);
 // router.route("/updatePrivacy").post(updatePrivacy);
 
 router.use(protect);
@@ -48,9 +48,9 @@ router
   .get(authorize("admin"), getUsers)
   .post(authorize("admin"), createUser);
 router.route("/find/:phone").get(protect, findPhone);
-router.route("/giftUser").post(protect, findPhoneByGift);
-router.route("/invoice/:id").post(invoiceTime);
-router.route("/giftInvoice/:id").post(invoiceGift);
+// router.route("/giftUser").post(protect, findPhoneByGift);
+// router.route("/invoice/:id").post(invoiceTime);
+// router.route("/giftInvoice/:id").post(invoiceGift);
 router.route("/delete").delete(protect, deleteUser);
 router.route("/me").get(protect, authMeUser);
 router.route("/:id").get(getUser).put(updateUser).delete(protect, deleteUser);
